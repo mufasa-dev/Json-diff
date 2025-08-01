53daa8476538a0e785008c2a3a21273770c139a3
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-08-01 16:37:14 -0300

    feat: ajust import of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: [
+    './src/**/*.{html,ts,scss}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
