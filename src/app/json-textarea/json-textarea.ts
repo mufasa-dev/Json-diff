2414968d6124b1dfe584ebce843201121ada46a5
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-08-01 16:37:42 -0300

    feat: create json textarea component

---
+++ src/app/json-textarea/json-textarea.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-json-textarea',
+  templateUrl: './json-textarea.html',
+  styleUrl: '../app.scss'
+})
+export class JsonTextArea {
+  
+}
