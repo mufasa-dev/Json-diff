9978bd200ed9350134d96afc1ff626777cbc6a13
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-08-05 20:35:15 -0300

    feat: create function compare json lines

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,34 @@ import { JsonTextArea } from './json-textarea/json-textarea';
 export class App {
   public json1 = "";
   public json2 = "";
+
+  public showResult: boolean = false;
+  public diffHtml1: string = '';
+  public diffHtml2: string = '';
+
+  public compareJsonLines(): void {
+    const lines1 = this.json1.split('\n');
+    const lines2 = this.json2.split('\n');
+    const maxLength = Math.max(lines1.length, lines2.length);
+
+    let result1 = '';
+    let result2 = '';
+
+    for (let i = 0; i < maxLength; i++) {
+      const line1 = lines1[i] ?? '';
+      const line2 = lines2[i] ?? '';
+      if (line1 === line2) {
+        result1 += `<div>${line1}</div>`;
+        result2 += `<div>${line2}</div>`;
+      } else {
+        result1 += `<div style="background-color: #a5ffb0">${line1}</div>`;
+        result2 += `<div style="background-color: #a5ffb0">${line2}</div>`;
+      }
+    }
+    console.log(lines1);
+    console.log(result2);
+    this.diffHtml1 = result1;
+    this.diffHtml2 = result2;
+    this.showResult = true;
+  }
 }
