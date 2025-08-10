cf192a368b14940a887382bd97bdb6e0bb97b400
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-08-09 23:35:39 -0300

    fix: ajust function get js objects

--- src/app/json-textarea/json-textarea.ts
+++ src/app/json-textarea/json-textarea.ts
@@ -26,23 +26,31 @@ export class JsonTextArea {
         try {
           let text = reader.result as string;
 
+          // Se for JS ou TS, extrai apenas o objeto exportado
           if (/\.(ts|js)$/i.test(file.name)) {
-            const exportRegex = /export\s+(?:const|default|class|let|var)\s+\w+(?:\s*:\s*[\w<>,\s\[\]\?]+)?\s*=\s*(\{[\s\S]*\})\s*(?:;|$)/m;
+            const exportRegex =
+              /export\s+(?:const|default|class|let|var)\s+\w+(?:\s*:\s*[\w<>,\s\[\]\?]+)?\s*=\s*(\{[\s\S]*\})\s*(?:;|$)/m;
             const match = text.match(exportRegex);
 
             if (!match) {
-              throw new Error('No exported objects found in the file.');
+              throw new Error("No exported objects found in the file.");
             }
 
             text = match[1];
+
+            // Converte o objeto JS/TS para um objeto real
+            const obj = new Function(`return (${text})`)();
+
+            // Emite como string JSON
+            this.jsonChange.emit(JSON.stringify(obj, null, 2));
+          } else {
+            // JSON puro
+            const obj = JSON.parse(text);
+            this.jsonChange.emit(JSON.stringify(obj, null, 2));
           }
 
-          JSON.parse(text);
-
-          this.jsonChange.emit(text);
-
           if (this.fileInput) {
-            this.fileInput.nativeElement.value = '';
+            this.fileInput.nativeElement.value = "";
           }
         } catch (err) {
           alert(`Invalid file: ${(err as Error).message}`);
